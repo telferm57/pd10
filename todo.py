c9d0dd9e06a4f968ae5346fc58d5b0da0fd3e3d7
telferm57 <telfermalcolm@yahoo.co.uk>
2017-06-12 10:14:19 +0100

    Add files via upload

--- todo.py
+++ todo.py
@@ -11,5 +11,10 @@ A. upgrade synapse client to latest
 B. move to picking individual files based on demographics rather than dowloading the whole lot
 C. add years since onset 
 D. get rid of globals 
+E. rewrite bits as class ? File handler ? 
+F. getscores and demographics - remove need to call separately if not files not loaded
+-- AT THE MOMENT YOU HAVE TO CALL THEM WITH 0 TO LOAD THE DATASET 
+PRIOR TO using the function to query the data 
+G get updrs score closest to date of file it is being added to 
 
 '''
